8ce5790050ed2df7d62dde721f6d035479a5763d
bnj-dez <designebenjamin@gmail.com>
2023-06-19 16:30:33 +0200

    correction string

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -2,10 +2,6 @@ export const login = (
   req,
   res
 ) => {
-  try {
-    req.session.user = 'toto';
-    return res.send('toto');
-  } catch (error) {
-    return res.status(500).send("Une erreur s'est produite lors de la récupération des livres");
-  }
+  req.session.user = "toto";
+  return res.send("L'utilisateur est connecté en tant que 'toto'");
 }
